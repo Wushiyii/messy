0fc1dfefabe90fa0ab59b4c6068af0d12c3d92d8
wuguoqing <wuguoqing@qudian.com>
2019-10-09 17:53:52 +0800

    add trace util

---
+++ src/main/java/com/wushiyii/messy/MessyApplication.java
@@ -0,0 +1,13 @@
+package com.wushiyii.messy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/wushiyii/messy/utils/TraceUtil.java
@@ -0,0 +1,15 @@
+package com.wushiyii.messy.utils;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TraceUtil {
+
+    //计数器 从1000累加
+    private static AtomicInteger count = new AtomicInteger(1000);
+    //十六进制IP地址
+    private static String IP_16 = "ffffffff";
+    //IP地址
+    private static String IP = "0.0.0.0";
+
+
+}
