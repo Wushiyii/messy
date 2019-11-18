03bfac88bde519acd835d0c2246f45e215ff6c41
jessyy <wgq287676521@qq.com>
2019-11-18 16:21:41 +0800

    Add Nio client comment

--- src/main/java/com/wushiyii/messy/grammar/netty/echo/EchoClient.java
+++ src/main/java/com/wushiyii/messy/grammar/netty/echo/EchoClient.java
@@ -13,25 +13,30 @@ import java.net.InetSocketAddress;
 
 public class EchoClient {
 
-    private final int PORT = 8088;
+    private final static int PORT = 8088;
 
     public void run() throws Exception {
         EventLoopGroup group = new NioEventLoopGroup();
         try {
             Bootstrap b = new Bootstrap();
             b.group(group)
-                    .channel(NioSocketChannel.class)
+                    .channel(NioSocketChannel.class)//指定使用NIO客户端channel
                     .remoteAddress(new InetSocketAddress(PORT))
                     .handler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new EchoClientHandler());
+                            ch.pipeline().addLast(new EchoClientHandler());//当建立通道时，创建客户端请求处理器实例到通道pipeline中
                         }
                     })
                     .option(ChannelOption.SO_KEEPALIVE, true);
+            //连接到远程，阻塞等待连接完成
             ChannelFuture f = b.connect().sync();
+
+            //一直阻塞，直到channel关闭
             f.channel().closeFuture().sync();
+
         } finally {
+            //阻塞等待关闭线程池等资源
           group.shutdownGracefully().sync();
         }
     }
