1d8edcb655d163c3378de7bfdd56a60410261a6e
jessyy <wgq287676521@qq.com>
2019-12-23 16:51:33 +0800

    增加min/max/distinct/match

--- src/main/java/com/wushiyii/messy/grammar/stream/OperateStreamDemo.java
+++ src/main/java/com/wushiyii/messy/grammar/stream/OperateStreamDemo.java
@@ -3,6 +3,7 @@ package com.wushiyii.messy.grammar.stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -116,13 +117,54 @@ public class OperateStreamDemo {
      * 这能帮助程序明显缩短执行时间。
      */
    private void sortedDemo() {
-       Stream<Integer> stream1 = Stream.of(5, 4, 2, 3, 1, 7);
-       stream1.sorted().forEach(System.out::println);
+        Stream<Integer> stream1 = Stream.of(5, 4, 2, 3, 1, 7);
+        stream1.sorted().forEach(System.out::println);
    }
+
+    /**
+     * min 和 max 的功能也可以通过对 Stream 元素先排序，再 findFirst 来实现，
+     * 但前者的性能会更好，为 O(n)，而 sorted 的成本是 O(n log n)。
+     * 同时它们作为特殊的 reduce 方法被独立出来也是因为求最大最小值是很常见的操作。
+     */
+   private void maxMinDistinctDemo(){
+       IntStream intStream1 = IntStream.of(5, 4, 2, 3, 1, 7);
+       intStream1.max().ifPresent(System.out::println);
+
+       IntStream intStream2 = IntStream.of(5, 4, 2, 3, 1, 7);
+       intStream2.min().ifPresent(System.out::println);
+
+       IntStream intStream3 = IntStream.of(1, 1, 1, 2, 2, 3);
+       intStream3.distinct().forEach(System.out::println);
+   }
+
+    /**
+     * Match：匹配操作
+     * predicate:预测的规则
+     * allMatch：Stream 中全部元素符合传入的 predicate，返回 true
+     * anyMatch：Stream 中只要有一个元素符合传入的 predicate，返回 true
+     * noneMatch：Stream 中没有一个元素符合传入的 predicate，返回 true
+     */
+   private void matchDemo() {
+       //判断是否全都匹配xxx
+       Stream<Integer> stream1 = Stream.of(1, 2, 3, 5, 7);
+       final boolean isAllPositive = stream1.allMatch(x -> x > 0);
+       System.out.println(isAllPositive);
+
+       //判断是否有任意一个匹配xxx
+       Stream<Integer> stream2 = Stream.of(1, 2, 3, 5, 7);
+       final boolean ifContainsOne = stream2.anyMatch(x -> x == 1);
+       System.out.println(ifContainsOne);
+
+       //判断是否所有都不匹配xxx
+       Stream<Integer> stream3 = Stream.of(1, 2, 3, 5, 7);
+       final boolean isNotContainsZero = stream3.noneMatch(x -> x == 0);
+       System.out.println(isNotContainsZero);
+   }
+
 
     public static void main(String[] args) {
         OperateStreamDemo demo = new OperateStreamDemo();
-        demo.sortedDemo();
+        demo.matchDemo();
     }
 
 
