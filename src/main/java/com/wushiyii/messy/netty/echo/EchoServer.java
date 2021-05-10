497e2d735e81838199700794b9b2e927830d7234
jessyy <wgq287676521@qq.com>
2021-05-10 18:11:29 +0800

    test

--- src/main/java/com/wushiyii/messy/netty/echo/EchoServer.java
+++ src/main/java/com/wushiyii/messy/netty/echo/EchoServer.java
@@ -8,7 +8,6 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
 /**
